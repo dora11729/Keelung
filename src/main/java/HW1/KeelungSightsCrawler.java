7a1b0df34aafb57dfd766506a2a64e5c59a98d9a
dora11729 <dora11729@gmail.com>
2025-03-10 02:30:52 +0800

    1.修正HW1輸入之有"區"無"區"皆可接受 2.增加HW1註解

--- src/main/java/HW1/KeelungSightsCrawler.java
+++ src/main/java/HW1/KeelungSightsCrawler.java
@@ -9,31 +9,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class KeelungSightsCrawler {
+    //總覽網址
     private static final String BASE_URL = "https://www.travelking.com.tw";
 
     public List<Sight> getItems(String targetZone) {
         List<Sight> sightsList = new ArrayList<>();
 
+        //如果沒有輸入，直接返回
         if (targetZone == null || targetZone.isEmpty()) {
             System.out.println("該區不存在");
             return sightsList;
         }
+        //如果輸入沒有加"區"，自動加上
+        if (!targetZone.endsWith("區")){
+            targetZone += "區";
+        }
 
         try {
             Document doc = Jsoup.connect(BASE_URL + "/tourguide/taiwan/keelungcity/").timeout(10000).get();
+
+            //找到並取得"基隆景點列表"區塊
             Element wrapper = doc.select("#guide-point .box").first();
             Elements zones = wrapper.select("h4");
             Elements sights = wrapper.select("ul");
 
+            //取得目標區每個景點資料，並將其儲存
             for (int i = 0; i < zones.size(); i++){
                 String zone = zones.get(i).text().trim();
+
+                //非目標區直接跳過
                 if (!zone.equals(targetZone)) {
-                    //System.out.println("pass: " + zone);
                     continue;
                 }
 
                 Elements zoneSights = sights.get(i+1).select("li a");
                 if (zoneSights != null){
+                    //已找到目標區，取得其下每個景點，並深入該景點頁面取得更多景點資訊
                     for (Element zoneSight: zoneSights){
                         String sightName = zoneSight.text().trim();
                         String sightURL = BASE_URL + zoneSight.attr("href"); // 景點的 URL
