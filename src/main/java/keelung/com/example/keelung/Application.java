6709c248e1c67b06e2151e6569229b143ab99e62
dora11729 <dora11729@gmail.com>
2025-06-13 20:30:23 +0800

    delete dotenv

--- src/main/java/keelung/com/example/keelung/Application.java
+++ src/main/java/keelung/com/example/keelung/Application.java
@@ -1,6 +1,5 @@
 package keelung.com.example.keelung;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -11,15 +10,6 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 public class Application {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-		String uri = dotenv.get("MONGODB_URI");
-		if (uri != null) {
-			System.setProperty("MONGODB_URI", uri);
-			System.out.println("✅ MONGODB_URI 載入成功: " + uri);
-		} else {
-			System.err.println("❌ 沒有從 .env 讀到 MONGODB_URI！");
-		}
-
 		SpringApplication.run(Application.class, args);
 	}
 
